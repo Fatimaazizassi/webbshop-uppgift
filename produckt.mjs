cd1c5176a4480ee5d66a7f31089d604bbe4b4213
Fatima Assi <Fatima.assi@medieinstitutet.se>
2023-11-27 13:55:28 +0100

    import produckt fil

---
+++ produckt.mjs
@@ -0,0 +1,205 @@
+
+
+
+const chipses = [
+    {
+      name:'Olw Hot Cheddar Chips',
+      price:23,
+      img:
+        {
+          src:'images/chips1.jpg',
+          alt:'Olw Hot Cheddar Chips',
+          width: 300,
+          height: 450,
+          
+    
+        },
+           
+      
+      rating:3,
+      amount: 0,
+      category:'snack',
+      description:'Flavored chips popular OLW cheez cruncherz flamin',
+    },
+    
+    {
+      name:'Ostbagar Cheez Doodles ',
+      price:29,
+      img:
+        {
+            src:'images/Ostbagar.jpg',
+            alt:'Ostbagar Cheez Doodles ',
+            width: 300,
+          height: 450,
+          },
+               
+      rating:3,
+      amount: 0,
+      category:'snack',
+      description:'Brittle and crispy corn rings flavored with mild cheese seasoning.',
+    },
+    {
+      name: 'Doritos Hot Chili  ',
+      price:32,
+      img:
+      {
+        src:'images/chips2.jpg',
+        alt:'Tortillachips Sweet Chili & Pepper ',
+        width: 300,
+        height: 450,
+      },
+         
+      rating:3,
+      amount: 0,
+      category:'snack',
+      description:'tortilla chip made from freshly roasted corn, with the delicious taste of sweet chili peppe',
+    },
+    
+    {
+      name: 'Chips Ranch & sourcream',
+      price:31,
+      img:
+      {
+        src:'images/Chips Ranch.jpg',
+        alt:'Estrella Ranch & Sourcream är finräfflade chips 275g',
+        width: 300,
+          height: 450,
+      },
+         
+      rating:3,
+      amount: 0,
+      category:'snack',
+      description:'Finely grooved potato chips with garlic, tomato, cheese, onion and sour cream flavour.',
+    },
+    
+    {
+      name: 'Olw Chips Havssalt ',
+      price:32,
+      img:
+      {
+        src:'images/Olw Chips Havssalt.jpg',
+        alt:'Olw Chips Havssalt & svartpeppar',
+        width: 300,
+        height: 450,
+      },
+         
+      rating:3,
+      amount: 0,
+      category:'snack',
+      description:'Coarsely grooved with daring tones of sea salt & black pepper!',
+    },
+    
+    
+    {
+      name: 'Ostbagar Cruncherz ',
+      price:32,
+      img:
+      {
+        src:'images/Ostbagar 2.jpg',
+        alt:'Tortillachips Sweet Chili & Pepper ',
+        width: 300,
+        height: 450,
+    
+      },
+         
+      rating:3,
+      amount: 0,
+      category:'snack',
+      description:'Cheese bows with the same good taste as the original cheez doodles but extra crispy and crunchy',
+    },
+    
+    {
+      name:'Chips Vickning',
+      price:33,
+      img:
+        {
+          src:'images/Chips Vickning.jpg',
+          alt:' krispiga grovräfflade potatischips med lätt sälta 275g',
+          width: 300,
+          height: 450,
+    
+        },
+           
+      
+      rating:3,
+      amount: 0,
+      category:'snack',
+      description:'Ribbed potato chips, lightly salted 250g',
+    },
+    {
+    name:'Lays Chips & Onion ',
+    price:23,
+    img:
+      {
+        src:'images/lays.jpg',
+        alt:'Sour Cream & Onion ',
+        width: 300,
+        height: 450,
+    
+      },
+         
+    
+    rating:3,
+    amount: 0,
+    category:'snack',
+    description:'Lays Chips Sour Cream & Onion ',
+    },
+    {
+    name:'Doritos Sweet Chili  ',
+    price:29,
+    img:
+      {
+        src:'images/doritos.jpg',
+        alt:'Tortillachips Sweet Chili & Pepper 170g',
+        width: 300,
+        height: 450,
+    
+      },
+         
+    
+    rating:3,
+    amount: 0,
+    category:'snack',
+    description:'tortilla chip made from freshly roasted corn, with the delicious taste of sweet chili pepper',
+    },
+    {
+      name:'Nachochips ',
+      price:16,
+      img:
+        {
+          src:'images/Nachochips.jpg',
+          alt:'nachosallad med, gräddfil, rädisor och tacokryddad färs.',
+          width: 300,
+          height: 450,
+        },
+           
+      
+      rating:3,
+      amount: 0,
+      category:'snack',
+      description:'Nachos are baked on corn flour and then deep fried',
+      },
+      {
+        name:'Estrella Dill Chips  ',
+        price:34,
+        img:
+          {
+            src:'images/estrella.jpg',
+            alt:' chips med dil smak',
+            width: 300,
+            height: 450,
+        
+          },
+             
+        
+        rating:3,
+        amount: 0,
+        category:'snack',
+        description:'Ribbed potato chips with dill and chive flavor from Estrella.',
+        },
+    
+    
+    
+    ];
+
+    export default chipses;
