5e73c215c7a3697f92892c1e4bae1291369e2d37
Fatima Assi <Fatima.assi@medieinstitutet.se>
2023-11-29 22:42:44 +0100

    bestäl & kasa

--- main.js
+++ main.js
@@ -1,20 +1,208 @@
  import './style.scss';
 
- import chipses from "./produckt.mjs"; // import produckt fil
+ //chipsContainer//
+const chipsHtmlContainer = document. querySelector('#chipsesContainer');
+const cartHtmlContainer = document.querySelector('#cart');
+const chipsContainer = document.querySelector('#chips');
 
-
- // chenge page
- const themeToggle = document.querySelector('#themeToggle');
- // add Event
- themeToggle.addEventListener('click', chengePage);
-
- function chengePage() {
-  if (document.body.classList.contains('dark-theme')) {
-    document.body.classList.remove('dark-theme');  
-  } else {
-    document.body.classList.add('dark-theme');
-  }
- }
+const chipses = [
+  {
+    name:'Olw Hot Cheddar Chips',
+    price:23,
+    img:
+      {
+        src:'images/chips1.jpg',
+        alt:'Olw Hot Cheddar Chips',
+        width: 300,
+        height: 450,
+        
+  
+      },
+         
+    
+    rating:3,
+    amount: 0,
+    category:'snack',
+    description:'Flavored chips popular OLW cheez cruncherz flamin',
+  },
+  
+  {
+    name:'Ostbagar Cheez Doodles ',
+    price:29,
+    img:
+      {
+          src:'images/Ostbagar.jpg',
+          alt:'Ostbagar Cheez Doodles ',
+          width: 300,
+        height: 450,
+        },
+             
+    rating:3,
+    amount: 0,
+    category:'snack',
+    description:'Brittle and crispy corn rings flavored with mild cheese seasoning.',
+  },
+  {
+    name: 'Doritos Hot Chili  ',
+    price:32,
+    img:
+    {
+      src:'images/chips2.jpg',
+      alt:'Tortillachips Sweet Chili & Pepper ',
+      width: 300,
+      height: 450,
+    },
+       
+    rating:3,
+    amount: 0,
+    category:'snack',
+    description:'tortilla chip made from freshly roasted corn, with the delicious taste of sweet chili peppe',
+  },
+  
+  {
+    name: 'Chips Ranch & sourcream',
+    price:31,
+    img:
+    {
+      src:'images/Chips Ranch.jpg',
+      alt:'Estrella Ranch & Sourcream är finräfflade chips 275g',
+      width: 300,
+        height: 450,
+    },
+       
+    rating:3,
+    amount: 0,
+    category:'snack',
+    description:'Finely grooved potato chips with garlic, tomato, cheese, onion and sour cream flavour.',
+  },
+  
+  {
+    name: 'Olw Chips Havssalt ',
+    price:32,
+    img:
+    {
+      src:'images/Olw Chips Havssalt.jpg',
+      alt:'Olw Chips Havssalt & svartpeppar',
+      width: 300,
+      height: 450,
+    },
+       
+    rating:3,
+    amount: 0,
+    category:'snack',
+    description:'Coarsely grooved with daring tones of sea salt & black pepper!',
+  },
+  
+  
+  {
+    name: 'Ostbagar Cruncherz ',
+    price:32,
+    img:
+    {
+      src:'images/Ostbagar 2.jpg',
+      alt:'Tortillachips Sweet Chili & Pepper ',
+      width: 300,
+      height: 450,
+  
+    },
+       
+    rating:3,
+    amount: 0,
+    category:'snack',
+    description:'Cheese bows with the same good taste as the original cheez doodles but extra crispy and crunchy',
+  },
+  
+  {
+    name:'Chips Vickning',
+    price:33,
+    img:
+      {
+        src:'images/Chips Vickning.jpg',
+        alt:' krispiga grovräfflade potatischips med lätt sälta 275g',
+        width: 300,
+        height: 450,
+  
+      },
+         
+    
+    rating:3,
+    amount: 0,
+    category:'snack',
+    description:'Ribbed potato chips, lightly salted 250g',
+  },
+  {
+  name:'Lays Chips & Onion ',
+  price:23,
+  img:
+    {
+      src:'images/lays.jpg',
+      alt:'Sour Cream & Onion ',
+      width: 300,
+      height: 450,
+  
+    },
+       
+  
+  rating:3,
+  amount: 0,
+  category:'snack',
+  description:'Lays Chips Sour Cream & Onion ',
+  },
+  {
+  name:'Doritos Sweet Chili  ',
+  price:29,
+  img:
+    {
+      src:'images/doritos.jpg',
+      alt:'Tortillachips Sweet Chili & Pepper 170g',
+      width: 300,
+      height: 450,
+  
+    },
+       
+  
+  rating:3,
+  amount: 0,
+  category:'snack',
+  description:'tortilla chip made from freshly roasted corn, with the delicious taste of sweet chili pepper',
+  },
+  {
+    name:'Nachochips ',
+    price:16,
+    img:
+      {
+        src:'images/Nachochips.jpg',
+        alt:'nachosallad med, gräddfil, rädisor och tacokryddad färs.',
+        width: 300,
+        height: 450,
+      },
+         
+    
+    rating:3,
+    amount: 0,
+    category:'snack',
+    description:'Nachos are baked on corn flour and then deep fried',
+    },
+    {
+      name:'Estrella Dill Chips  ',
+      price:34,
+      img:
+        {
+          src:'images/estrella.jpg',
+          alt:' chips med dil smak',
+          width: 300,
+          height: 450,
+      
+        },
+           
+      
+      rating:3,
+      amount: 0,
+      category:'snack',
+      description:'Ribbed potato chips with dill and chive flavor from Estrella.',
+      },
+  
+  ];
 
   // kort & faktora
   const cardInvoiceRadios = Array.from(document.querySelectorAll('input[name="payment-option"]'));
@@ -105,11 +293,6 @@
  
 }
 
-//chipsContainer//
-const chipsHtmlContainer = document. querySelector('#chipsesContainer');
-const cartHtmlContainer = document.querySelector('#cart');
-const chipsContainer = document.querySelector('#chips');
-
 
 const today = new Date();
 
@@ -171,6 +354,8 @@ function printchipses(){
       <div class="amount">Amount: <span>${chips.amount}</span> </div>
       <button class="minus" data-id="${index}"> - </button>
       <button class="plus" data-id="${index}"> + </button>
+     <span> ${chips.amount * chips.price} kr - Standard pris</span>
+
   
       </article>
     `;
@@ -238,8 +423,49 @@ function printCartchipses(){
   } else{
 
   cartHtmlContainer.innerHTML += `<p class="topay"> Shipping: ${Math.round(25 + (0.1 * sum))} Kr</p>`;
-
+  
   }
+}
+
+
+printchipses();
+
+
+//button Beställning
+const orderBtnClick = document.querySelector('#orderBtnClick');
+orderBtnClick.addEventListener('click', showOrder);
+
+function showOrder(){
+  // dölj besällningsformuläret
+  const orderForm =document.querySelector('#orderForm')
+  orderForm.classList.add('hidden');
+
+  // vissa beställnings bäkreftelsen
+  const orderConfirmation =document.querySelector('#orderConfirmation')
+  orderConfirmation.classList.remove('hidden');
+
+  //hämta namn
+  const clienName = document.querySelector('#firstName').value;
+  document.querySelector('#nameFirst').innerHTML=clienName;
 
 }
-printchipses();
+
+
+
+const cartButton = document.querySelector('#cartButton');
+cartButton.addEventListener('click',cartButtonClick);
+
+function cartButtonClick() {
+ 
+  document.getElementById("chipsesContainer").style.display = "none";
+  document.getElementById("money").style.display = "block";
+}
+
+const goBack = document.querySelector('#goBack');
+goBack.addEventListener('click',backPage);
+
+function backPage() {
+  document.getElementById("chipsesContainer").style.display = "block";
+  document.getElementById("money").style.display = "none";
+}
+
